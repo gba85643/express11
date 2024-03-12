18f801312199b4aced49758d1e27ea037ad88081
Sun <gba85643@gmail.com>
2024-03-12 14:52:40 +0800

    變更HTML

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const express = require('express');
-const {resolve} = require('path');
+const express = require("express");
+const path = require("path");
 
+// const __dirname = path.resolve();
 
 const app = express();
-app.use(express.static(resolve(__dirname, "public")));
-app.use("/bootstrap", express.static(resolve(__dirname, "node_modules/bootstrap/dist")));
-app.use("/jquery", express.static(resolve(__dirname, "node_modules/jquery/dist")))
-app.use('/fontawesome', express.static(resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
+app.use(express.static(path.resolve(__dirname, "public")));
+app.use("/bootstrap", express.static(path.resolve(__dirname, "node_modules/bootstrap/dist")));
+app.use("/jquery", express.static(path.resolve(__dirname, "node_modules/jquery/dist")));
+app.use('/fontawesome', express.static(path.resolve(__dirname, 'node_modules/@fortawesome/fontawesome-free')));
 
 app.get("/", (req, res) => {
-  res.send("網站首頁")
+  res.send("網站首頁");
 });
 
-
 app.listen(3000, () => {
-  ;console.log("伺服器已啟動於 http://localhost:3000");
-})
+  console.log("伺服器已啟動於 http://localhost:3000");
+});
